a988f4ca077522f3cde29f51c3065e9dea2908e4
PocketWilds <austin.ascura@gmail.com>
2023-06-29 14:53:20 -0700

    replacing old raw analysis function

--- video_analyzer.py
+++ video_analyzer.py
@@ -83,11 +83,11 @@ class VideoAnalyzer:
         if(self._video != None and self.filepath != None):
             self._video = cv2.VideoCapture(self.filepath)
         
-        raw_results, frame_count, fps = self._test_analysis(frame_ranges)#self._get_raw_video_analysis(frame_ranges)
+        raw_results, frame_count, fps = self._get_raw_video_analysis(frame_ranges)
         print(frame_count)
         meta_results = self._get_meta_analysis(raw_results, frame_count, fps)
 
-    def _test_analysis(self, frame_ranges, scale_factor=1.0, monitored_section=(1638, 70, 1852, 570)):
+    def _get_raw_video_analysis(self, frame_ranges, scale_factor=1.0, monitored_section=(1638, 70, 1852, 570)):
         frame_comparisons = []
         next_available_trigger = 0
         for frame_range in frame_ranges:
@@ -132,49 +132,6 @@ class VideoAnalyzer:
 
         return frame_comparisons, self._video.get(cv2.CAP_PROP_FRAME_COUNT), self._video.get(cv2.CAP_PROP_FPS)
     
-    def _get_raw_video_analysis(self, frame_ranges, scale_factor=1.0, monitored_section=(1638, 70, 1852, 570)):
-        frames_queue = queue.Queue()
-        frame_comparisons = []
-        
-        next_available_trigger = 0
-        while (True):
-            read_result, frame = self._video.read()
-            if read_result:
-                current_frame = self._video.get(cv2.CAP_PROP_POS_FRAMES)
-                src_img = Image.fromarray(frame)
-                crop = src_img.crop(monitored_section)
-                frame = np.asarray(crop)
-                shape = frame.shape
-                scaled_h = int(shape[0] * scale_factor)
-                scaled_w = int(shape[1] * scale_factor)
-                frame = cv2.resize(frame, dsize=(scaled_w, scaled_h), interpolation=cv2.INTER_CUBIC)
-                
-                if current_frame > 1.0:
-                    isnt_locked = current_frame > next_available_trigger
-                    mse_result = VideoAnalyzer.mse(frame, past_frame)
-                    ssim_result = VideoAnalyzer.ssim(frame, past_frame)
-                    has_new_msg = mse_result > 11000.0 and ssim_result < 0.55 and isnt_locked
-                    if(has_new_msg):
-                        next_available_trigger = current_frame + 19.0
-                    frame_comparisons.append((current_frame, mse_result, ssim_result, has_new_msg))
-                #name = "..video-bites-data/data/" + str(current_frame) + '.png'
-                #cv2.imwrite(name, scaledFrame)
-                past_frame = frame
-
-                #print("adding frame #" + str(current_frame))
-                pass
-            else:
-                break
-
-        writeFile ="./simplified-detection.txt"
-        file = open(writeFile, 'w')
-        frame_comparisons_str = ""
-        for tup_elem in frame_comparisons:
-            file.write(str(tup_elem[0]) + "\t" + str(tup_elem[1]) + "\t" + str(tup_elem[2]) + "\t" + str(tup_elem[3]) + "\n")
-        file.close()
-
-        return frame_comparisons, self._video.get(cv2.CAP_PROP_FRAME_COUNT), self._video.get(cv2.CAP_PROP_FPS)
-
     def _get_meta_analysis(self, raw_results, frame_count, fps):
         trigger_points = list(filter(lambda x:x[3] == True, raw_results))
         total_triggers = len(trigger_points)
