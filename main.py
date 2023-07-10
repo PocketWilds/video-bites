7f2e72af99acc8c6eea811dd7c9d51fea2ba8a6d
pocketwilds <austin.ascura@gmail.com>
2023-07-09 21:49:02 -0700

    restructuring main to log errors in the event of catastrophe

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from gui import Gui
 import time
 
-if __name__ == "__main__":
-    start_time = time.perf_counter()
-    gui = Gui()
+from gui import Gui
+from log_manager import LogManager
 
+if __name__ == '__main__':
+    start_time = time.perf_counter()
+    try:
+        raise Exception(error_message)
+        gui = Gui()
+    except Exception as e:
+        LogManager.write_error_log(e)
