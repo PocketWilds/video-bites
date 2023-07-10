8b08313d3de2c20246dbb1eba7baedacf48c2f6a
pocketwilds <austin.ascura@gmail.com>
2023-07-09 21:53:33 -0700

    removing old test code

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from log_manager import LogManager
 if __name__ == '__main__':
     start_time = time.perf_counter()
     try:
-        raise Exception(error_message)
         gui = Gui()
     except Exception as e:
         LogManager.write_error_log(e)
