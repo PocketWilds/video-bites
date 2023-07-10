e693a50c0bfab1fb14b9df112f8c7d6c66f54cd7
pocketwilds <austin.ascura@gmail.com>
2023-07-10 01:24:43 -0700

    removed old performance testing scaffolding

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-
 from gui import Gui
 from log_manager import LogManager
 
 if __name__ == '__main__':
-    start_time = time.perf_counter()
     try:
         gui = Gui()
     except Exception as e:
