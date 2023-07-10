c11679b4afa705ecb7b9ddcf1c7aa0df3d9bd06f
pocketwilds <austin.ascura@gmail.com>
2023-07-10 00:36:18 -0700

    adjust step value for decay rate to allow for finer control

--- gui.py
+++ gui.py
@@ -142,7 +142,7 @@ class Gui:
                                             dpg.add_input_float(tag='PointValueInput', width = 130, default_value=2.0,step=1, min_value=0, min_clamped=True)
                                             dpg.add_text(default_value='Point Value')
                                             dpg.add_spacer(width=80)
-                                            dpg.add_input_float(tag='DecayRateInput', width = 130, default_value=0.002,step=0.001, min_value=0, min_clamped=True)
+                                            dpg.add_input_float(tag='DecayRateInput', width = 130, default_value=0.002,step=0.0001, min_value=0, min_clamped=True)
                                             dpg.add_text(default_value='Decay Rate (per frame)')
 
                     dpg.add_spacer(height=3)
